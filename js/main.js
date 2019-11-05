9d42ff4d785a3ef55a5ec98fae0c65b96e022e15
anvar nazar <anvrnazr@gmail.com>
2019-11-05 19:27:09 +0530

    footer fix

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ var body = document.querySelector('body');
 document.querySelector('.menubtn').onclick = function() {
     body.classList.toggle("menu-active");            
 }
+var x = document.getElementsByClassName('qua-btn');
+console.log(x);
+
+ function openQua(e) {
+
+    
+e.currentTarget.classList.add("active"); 
+e.currentTarget.previousSibling.classList.remove("active");
+
+}
+
+function openExp(e) {
+
+
+    e.currentTarget.classList.add("active"); 
+    e.currentTarget.nextSibling.classList.remove("active");
+    
+    }
